ddfd4d21dd59d28421293c3a8c3459ae4853dae6
Kirin <chakhriya.chantharakhami@studentambassadors.com>
2024-01-02 23:35:19 +0700

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -18,14 +18,11 @@ const Navbar = () => {
           />
         </a>
       </div>
-      <div className="flex items-center justify-end mx-3  sm:text-2xl lg:text-2xl">
+      <div className="flex items-center justify-end mx-3 sm:text-2xl lg:text-2xl">
         <a href="https://linktr.ee/epd_dmsu" className="text-white mx-5">
           สมัครเลย
         </a>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSe99AgZoeUsaege0ebSLLqXec2kaQUBbigjXsaSWVYbFOdDhA/viewform" className="text-white mx-5">
-          สั่งซื้อเสื้อ
-        </a>
-        <a href="https://www.facebook.com/profile.php?id=61554407915847" className="text-white mx-6">
+        <a href="https://www.facebook.com/profile.php?id=61554407915847" className="text-white mx-5">
           ติดต่อเรา
         </a>
       </div>
