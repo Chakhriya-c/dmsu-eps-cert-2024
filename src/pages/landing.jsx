93e4664358d9324826f6c23d4edc1d9b54eeee28
Kirin <chakhriya.chantharakhami@studentambassadors.com>
2024-01-03 11:25:55 +0700

    commit

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -56,11 +56,11 @@ const LandingPage = () => {
         ></h1>
       </div>
 
-      <div id="information" className='flex flex-col py-12 items-center justify-center sm:mx-32 sm:mt-48 sm:text-2xl lg:mt-52 xl:mt-64 lg:mx-40 xl:mx-60 lg:text-2xl xl:text-3xl '>
+      <div id="information" className='flex flex-col py-12 items-center  justify-center sm:mx-32 sm:mt-48 sm:text-2xl lg:mt-52 xl:mt-64 lg:mx-40 xl:mx-60 lg:text-2xl xl:text-3xl '>
         <p className='text-white py-5 '> 📅 วันที่ 10 กุมภาพันธ์ 2567 </p>
         <p className='text-white py-5 '> 📍  ชั้น 1 เสริมไทยคอมเพล็กซ์</p>
         <button className='bg-black text-black'>.</button>
-        <button href="https://linktr.ee/epd_dmsu" className='text-xl sm:text-3xl bg-indigo-600 hover:bg-indigo-500 text-white rounded-xl py-4 px-5'> <a href='https://linktr.ee/epd_dmsu'> สมัครเลย 🚀 </a></button>
+        <button href="https://linktr.ee/epd_dmsu" className='text-xl mt-1 sm:text-3xl bg-indigo-600 hover:bg-indigo-500 text-white rounded-xl py-4 px-5'> <a href='https://linktr.ee/epd_dmsu'> สมัครเลย 🚀 </a></button>
       </div>       
     <div className='mx-12'>
       <div id="rov" style={{
